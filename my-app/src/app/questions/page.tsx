ec482a69663adfa4c14c4cd3085ef98eea93bbe5
Kelly Penn <kepenn@kepenn-mac.natera.local>
2024-11-14 20:11:23 -0600

    created question page lol

---
+++ my-app/src/app/questions/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div>
+            <h1>Question page</h1>
+        </div>
+    );
+};
+
+export default Page;
+
+
+// const QuestionsPage = () => {
+    
+//     return (
+//         <div>
+//             {<h1>Hello</h1>}
+//         </div>
+//     );
+// }
+
+// export default QuestionsPage;
